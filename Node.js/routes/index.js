045dddef1e27ed4eb6751414c781e0f8b4aacff7
fOuttaMyPaint <154358121+fOuttaMyPaint@users.noreply.github.com>
2024-06-05 02:41:37 -0400

    Update index.js

--- Node.js/routes/index.js
+++ Node.js/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 router.get('/about', (req, res) => {
